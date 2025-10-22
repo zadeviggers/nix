dd5d513f9fc2d04edcb43266d04b3b573d700091
Zade Viggers <zade@viggers.net>
2025-10-22 13:36:06 +1300

    Fix godot

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
           "sf-symbols"
           "steam"
           "google-chrome@canary"
-          "godot@4.4"
+          "godot"
           "blender"
           "postgres-unofficial"
           "kdenlive"
